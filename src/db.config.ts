4f6950ebc6408a5ce0640abd4deb61eb11ba2e92
Khoa <khoa.ha@zodinet.tech>
2021-11-22 22:55:45 +0700

    config database

--- src/db.config.ts
+++ src/db.config.ts
@@ -1,12 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// export const DbConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'abcd1234',
+//   database: 'Login',
+//   entities: ['dist/**/entities/*.entity.js'],
+//   synchronize: true,
+// };
+
+// heroku
 export const DbConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-23-23-141-171.compute-1.amazonaws.com',
   port: 5432,
-  username: 'postgres',
-  password: 'abcd1234',
-  database: 'Login',
+  username: 'xnrkephmmdfzcf',
+  password: 'e930f7741eb7879f827a5682ce65d591ff00c50dda11286c7ecde1c107374b7d',
+  database: 'de27sgti00nel6',
   entities: ['dist/**/entities/*.entity.js'],
   synchronize: true,
 };
