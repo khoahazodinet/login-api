a3bfb1dc4ed1a05cfb8e0c389758a7cc4fdb30c2
Khoa <khoa.ha@zodinet.tech>
2021-11-22 23:23:34 +0700

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap().then(() => {
   console.log('the server had started');
