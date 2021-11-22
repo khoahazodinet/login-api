a0eebef0b3dd8782282e42121664ec4dac93e476
duong-leit <duong.le@zodinet.tech>
2021-11-22 23:29:28 +0700

    update recaptcha api and change folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,22 @@
 import { Module } from '@nestjs/common';
-import { LoginModule } from './auth/login.module';
+import { LoginModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DbConfig } from './db.config';
 import { GoogleRecaptchaModule, GoogleRecaptchaNetwork } from '@nestlab/google-recaptcha';
-import { reCaptLoginModule } from './login/login.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     GoogleRecaptchaModule.forRoot({
       secretKey: '6LeV9k4dAAAAABLSl4Z7N7wYFDQd7zCP_SGOqhNc',
-      response: req => req.body.recaptcha,
-      actions: ['login'],
-      score: 0.1
+      response: (req) => req.body.recaptcha,
+      score: 0.9
     }),
     TypeOrmModule.forRoot(DbConfig),
     LoginModule,
-    UserModule,
-    reCaptLoginModule
-  ],
+    UserModule
+  ]
 })
 export class AppModule {}
--- src/auth/services/login.service.ts
+++ src/auth/services/login.service.ts
@@ -1,49 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { TokenDto } from '../models/login.dto';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginRequestDto } from '../models/login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/services/user.service';
-import { IUser } from 'src/user/models/user.interface';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class LoginService {
-	constructor(private userService: UserService, private jwtService: JwtService) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
-	async validateUser(UserName: string, Password: string): Promise<any> {
-		const result = await this.userService.getUserByUserName(UserName);
-		if (result && result.length > 1) return null;
+  async validateUser(UserName: string, Password: string): Promise<any> {
+    const result = await this.userService.getUserByUserName(UserName);
+    if (result && result.length > 1) return null;
 
-		const user = result[0];
+    const user = result[0];
 
-		if (user && await bcrypt.compare(Password, user.Password)) {
-			const { Password, ...result } = user;
-			return result;
-		}
-		return null;
-	}
+    if (user && (await bcrypt.compare(Password, user.Password))) {
+      const { Password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
 
-	handleLogin(data: IUser): TokenDto {
-		const payload = { UserName: data.UserName, sub: data.ID };
-		return {
-			access_token: this.jwtService.sign(payload),
-		};
-	}
+  async handleLogin(data: LoginRequestDto) {
+    const user = await this.userService.findOne({ UserName: data.userName });
+    if (!user) {
+      throw new BadRequestException('Information is invalid.');
+    }
+    console.log('user', user);
+    if (await bcrypt.compare(data.password, user.Password)) {
+      const { ID, ...result } = user;
+      const payload = { userName: data.userName, sub: ID };
+
+      return this.jwtService.signAsync(payload);
+    }
+    throw new UnauthorizedException('password is invalid.');
+  }
 }
-
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { UserEntity } from '../../user/entities/user.entity';
-// import { Repository } from 'typeorm';
-// import { IUser } from '../../user/models/user.interface';
-
-// @Injectable()
-// export class LoginService {
-// 	constructor(
-// 		@InjectRepository(UserEntity)
-// 		private readonly userRepository: Repository<UserEntity>
-// 	) {}
-
-// 	async findOne(condition: { [key: string]: any }): Promise<IUser> {
-// 		return this.userRepository.findOne(condition);
-// 	}
-// }
